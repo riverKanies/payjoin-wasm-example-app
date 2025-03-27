4ddcea4f152835fb76fadc87013d1d86dc6ee299
River Kanies <river.kanies@gmail.com>
2025-03-27 11:24:57 -0500

    chore: cleanup, reorg

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ const payjoinDirectory = "https://payjo.in";
 
 
 // RUN
-// createAndSavePjUriAndPsbt();
-senderStep1();
+main();
+
+async function main() {
+    const receiver = await createAndSavePjUriAndPsbt();
+    const sendGetContext = await senderStep1();
+    console.log("preparing for receiver to add inputTx", receiver, sendGetContext);
+}
 
 
 async function senderStep1() {
@@ -45,7 +50,7 @@ async function senderStep1() {
     console.log(request);
     console.log(request.url);
     console.log(request.content_type);
-    console.log(request.body);
+    // console.log(request.body);
     const response = await fetch(request.url, {
         method: 'POST',
         headers: {
@@ -68,6 +73,8 @@ async function senderStep1() {
     // throws error bc post context is consumed
     // const sendGetContext2 = context.process_response(result);
     // console.log(sendGetContext2);
+
+    return sendGetContext;
 }
 
 
@@ -94,7 +101,7 @@ async function createAndSavePjUriAndPsbt() {
         ohttpRelay
     );
     console.log(receiver);
-    console.log(receiver.to_json());
+    // console.log(receiver.to_json());
     // got the pj_uri for the sender to use:
     const pjUriString = receiver.pj_uri().as_string
     console.log(pjUriString)
@@ -108,13 +115,12 @@ async function createAndSavePjUriAndPsbt() {
     console.log(psbt.fee_amount().to_sat());
     const psbtString = psbt.toString();
     console.log(psbtString);
-    const psbt2 = Psbt.from_string(psbtString);
-    console.log(psbt2.fee_amount().to_sat());
-    console.log(psbt2.to_json());
 
     // save to local storage
     localStorage.setItem("psbtString", psbtString);
     localStorage.setItem("pjUriString", pjUriString);
+
+    return receiver
 }
 
 
@@ -149,98 +155,3 @@ async function initSenderAndReceiverWallets() {
 
     return {senderWallet, receiverWallet};
 }
-
-
-// simple string storage example
-const Store = {
-    save: data => {
-        if (!data) {
-            console.log("No data to save");
-            return;
-        }
-        localStorage.setItem("walletData", data);  // data is already a JSON string
-    },
-    load: () => {
-        return localStorage.getItem("walletData");  // return the JSON string directly
-    }
-}
-
-const externalDescriptor = "tr([12071a7c/86'/1'/0']tpubDCaLkqfh67Qr7ZuRrUNrCYQ54sMjHfsJ4yQSGb3aBr1yqt3yXpamRBUwnGSnyNnxQYu7rqeBiPfw3mjBcFNX4ky2vhjj9bDrGstkfUbLB9T/0/*)#z3x5097m";
-const internalDescriptor = "tr([12071a7c/86'/1'/0']tpubDCaLkqfh67Qr7ZuRrUNrCYQ54sMjHfsJ4yQSGb3aBr1yqt3yXpamRBUwnGSnyNnxQYu7rqeBiPfw3mjBcFNX4ky2vhjj9bDrGstkfUbLB9T/1/*)#n9r4jswr";
-
-async function run() {    
-    let walletDataString = Store.load();
-    console.log("Wallet data:", walletDataString);
-
-    let wallet;
-    let client = new EsploraClient("https://mutinynet.com/api");
-    if (!walletDataString) {
-        console.log("Creating new wallet");
-        wallet = Wallet.create(
-            "signet",
-            externalDescriptor,
-            internalDescriptor
-        );
-
-        console.log("Performing Full Scan...");
-        let full_scan_request = wallet.start_full_scan();
-        let update = await client.full_scan(full_scan_request, 5, 1);
-        wallet.apply_update(update);
-
-        const stagedDataString = wallet.take_staged().to_json();
-        console.log("Staged:", stagedDataString);
-
-        Store.save(stagedDataString);
-        console.log("Wallet data saved to local storage");
-        walletDataString = stagedDataString;
-    } else {
-        console.log("Loading wallet");
-        let changeSet = ChangeSet.from_json(walletDataString);
-        wallet = Wallet.load(
-            changeSet,
-            externalDescriptor,
-            internalDescriptor
-        );
-
-        console.log("Syncing...");
-        let sync_request = wallet.start_sync_with_revealed_spks();
-        let update = await client.sync(sync_request, 1);
-        wallet.apply_update(update);
-
-        const updateChangeSet = wallet.take_staged();
-        if (updateChangeSet) {
-            console.log("Update:", updateChangeSet.to_json());
-            let currentChangeSet = ChangeSet.from_json(walletDataString);
-            console.log("Current:", currentChangeSet.to_json());
-            currentChangeSet.merge(updateChangeSet);
-            console.log("Merged:", currentChangeSet.to_json());
-            Store.save(currentChangeSet.to_json());
-        }
-    }
-
-    // Test balance
-    console.log("Balance:", wallet.balance().confirmed.to_sat());
-    
-    // Test address generation
-    console.log("New address:", wallet.reveal_next_address().address);
-
-
-    // handle merging
-    walletDataString = Store.load();
-    const updateChangeSet = wallet.take_staged();
-    console.log("Update:", updateChangeSet.to_json());
-    let currentChangeSet = ChangeSet.from_json(walletDataString);
-    console.log("Current:", currentChangeSet.to_json());
-    currentChangeSet.merge(updateChangeSet);
-    console.log("Merged:", currentChangeSet.to_json());
-    Store.save(currentChangeSet.to_json());
-    console.log("new address saved");
-}
-
-// run().catch(console.error);
-
-
-
-
-// to clear local storage:
-// localStorage.removeItem("walletData");
